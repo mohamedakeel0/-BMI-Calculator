1da9b44c2c12096003d8a2b36746f15807c13623
Your Name <you@example.com>
2022-06-25 23:59:06 +0200

    intial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:actevites/bmi_screen.dart';
+import 'package:flutter/material.dart';
+
+
+void main()
+{
+  runApp(MyApp());
+}
+
+
+
+class MyApp extends StatelessWidget
+{
+
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BmiScreen(),
+    );
+  }
+}
+
